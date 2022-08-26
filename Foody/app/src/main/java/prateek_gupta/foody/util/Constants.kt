2cef034000b330a22bee4963ad7a177615cfbd42
Prateek Gupta <prateek.gupta25apr@gmail.com>
2022-08-27 00:06:32 +0530

    Added chip_background_color.xml and chip_text_color.xml and updated recipes_bottom_sheet.xml, colors.xml and themes.xml

--- Foody/app/src/main/java/prateek_gupta/foody/util/Constants.kt
+++ Foody/app/src/main/java/prateek_gupta/foody/util/Constants.kt
@@ -8,8 +8,8 @@ class Constants {
 
         const val QUERY_NUMBER = "number"
         const val QUERY_API_KEY = "apiKey"
-        const val QUERY_TYPE = "type"
-        const val QUERY_DIET = "diet"
+        const val QUERY_TYPE = "main course"
+        const val QUERY_DIET = "gluten free"
         const val QUERY_ADD_RECIPE_INFORMATION = "addRecipeInformation"
         const val QUERY_FILL_INGREDIENTS = "fillIngredients"
 
