2c1f5ac1b71762d1a5d47ccbdfa85205c68c3489
Prateek Gupta <prateek.gupta25apr@gmail.com>
2022-08-30 08:37:39 +0530

    Updated RecipesBottomSheet.kt and RecipesViewModel.kt

--- Foody/app/src/main/java/prateek_gupta/foody/viewmodels/RecipesViewModel.kt
+++ Foody/app/src/main/java/prateek_gupta/foody/viewmodels/RecipesViewModel.kt
@@ -2,6 +2,11 @@ package prateek_gupta.foody.viewmodels
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import prateek_gupta.foody.data.DataStoreRepository
 import prateek_gupta.foody.util.Constants.Companion.API_KEY
 import prateek_gupta.foody.util.Constants.Companion.DEFAULT_DIET_TYPE
 import prateek_gupta.foody.util.Constants.Companion.DEFAULT_MEAL_TYPE
@@ -12,16 +17,38 @@ import prateek_gupta.foody.util.Constants.Companion.QUERY_DIET
 import prateek_gupta.foody.util.Constants.Companion.QUERY_FILL_INGREDIENTS
 import prateek_gupta.foody.util.Constants.Companion.QUERY_NUMBER
 import prateek_gupta.foody.util.Constants.Companion.QUERY_TYPE
+import javax.inject.Inject
 
-class RecipesViewModel(application: Application) : AndroidViewModel(application) {
+@HiltViewModel
+class RecipesViewModel @Inject constructor(
+    application: Application,
+    private val dataStoreRepository: DataStoreRepository
+) : AndroidViewModel(application) {
+
+    private var mealType = DEFAULT_MEAL_TYPE
+    private var dietType = DEFAULT_DIET_TYPE
+
+    val readMealAndDietType = dataStoreRepository.readMealAndDietType
+
+    fun saveMealAndDietType(mealType: String, mealTypeId: Int, dietType: String, dietTypeId: Int) =
+        viewModelScope.launch(Dispatchers.IO) {
+            dataStoreRepository.saveMealAndDietType(mealType, mealTypeId, dietType, dietTypeId)
+        }
 
     fun applyQueries(): HashMap<String, String> {
         val queries: HashMap<String, String> = HashMap()
 
+        viewModelScope.launch {
+            readMealAndDietType.collect { value ->
+                mealType = value.selectedMealType
+                dietType = value.selectedDietType
+            }
+        }
+
         queries[QUERY_NUMBER] = DEFAULT_RECIPES_NUMBER
         queries[QUERY_API_KEY] = API_KEY
-        queries[QUERY_TYPE] = DEFAULT_MEAL_TYPE
-        queries[QUERY_DIET] = DEFAULT_DIET_TYPE
+        queries[QUERY_TYPE] = mealType
+        queries[QUERY_DIET] = dietType
         queries[QUERY_ADD_RECIPE_INFORMATION] = "true"
         queries[QUERY_FILL_INGREDIENTS] = "true"
 
