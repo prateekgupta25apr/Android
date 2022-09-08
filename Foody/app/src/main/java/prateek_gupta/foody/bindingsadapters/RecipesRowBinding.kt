e8d0ac500764be89c51d52f2e0145adb97ee5274
Prateek Gupta <prateek.gupta25apr@gmail.com>
2022-09-09 00:35:38 +0530

    Added DetailsActivity and activity_details.xml and updated RecipesRowBinding, ExtendedIngredient, Result, recipes_row_layout.xml, my_nav.xml, colors.xml, themes.xml and AndroidManifest.xml

--- Foody/app/src/main/java/prateek_gupta/foody/bindingsadapters/RecipesRowBinding.kt
+++ Foody/app/src/main/java/prateek_gupta/foody/bindingsadapters/RecipesRowBinding.kt
@@ -1,16 +1,36 @@
 package prateek_gupta.foody.bindingsadapters
 
+import android.util.Log
 import android.view.View
 import android.widget.AutoCompleteTextView
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
+import androidx.navigation.findNavController
 import coil.load
 import prateek_gupta.foody.R
+import prateek_gupta.foody.models.Result
+import prateek_gupta.foody.ui.fragments.recipes.RecipesFragmentDirections
 
 class RecipesRowBinding {
     companion object{
+
+        @BindingAdapter("onRecipeClickListener")
+        @JvmStatic
+        fun onRecipeClickListener(recipeRowLayout: ConstraintLayout, result: Result) {
+            Log.d("onRecipeClickListener", "CALLED")
+            recipeRowLayout.setOnClickListener {
+                try {
+                    val action =
+                        RecipesFragmentDirections.actionRecipesFragmentToDetailsActivity(result)
+                    recipeRowLayout.findNavController().navigate(action)
+                } catch (e: Exception) {
+                    Log.d("onRecipeClickListener", e.toString())
+                }
+            }
+        }
 
         @BindingAdapter("loadImageFromUrl")
         @JvmStatic
