7026f560bf7589055798459ae3384171f0dccf4c
Prateek Gupta <prateek.gupta25apr@gmail.com>
2022-09-10 16:25:00 +0530

    Added PagerAdapter, IngredientsFragment, InstructionsFragment, OverviewFragment, fragment_ingredients.xml, fragment_instructions.xml and fragment_overview.xml and updated DetailsActivity

---
+++ Foody/app/src/main/java/prateek_gupta/foody/ui/fragments/ingredients/IngredientsFragment.kt
@@ -0,0 +1,17 @@
+package prateek_gupta.foody.ui.fragments.ingredients
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import prateek_gupta.foody.R
+class IngredientsFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_ingredients, container, false)
+    }
+}
