bdf92f90b3453dbd4b13818cf688b8a40f47e274
Prateek Gupta <prateek.gupta25apr@gmail.com>
2022-09-11 22:47:06 +0530

    Updated InstructionsFragment, Result, fragment_overview.xml, fragment_instructions.xml, recipes_row_layout.xml and AndroidManifest.xml

--- Foody/app/src/main/java/prateek_gupta/foody/ui/fragments/instructions/InstructionsFragment.kt
+++ Foody/app/src/main/java/prateek_gupta/foody/ui/fragments/instructions/InstructionsFragment.kt
@@ -5,13 +5,29 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebViewClient
+import kotlinx.android.synthetic.main.fragment_instructions.view.*
 import prateek_gupta.foody.R
+import prateek_gupta.foody.models.Result
+import prateek_gupta.foody.util.Constants
+
 class InstructionsFragment : Fragment() {
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_instructions, container, false)
+        val view = inflater.inflate(R.layout.fragment_instructions, container, false)
+
+        val args = arguments
+        val myBundle = args?.getParcelable(Constants.RECIPE_RESULT_KEY) as Result?
+
+        //view.instructions_webView.webViewClient = object : WebViewClient() {}
+        val websiteUrl: String = myBundle!!.sourceUrl
+        view.instructions_webView.loadUrl(websiteUrl)
+
+        return view
     }
+
 }
