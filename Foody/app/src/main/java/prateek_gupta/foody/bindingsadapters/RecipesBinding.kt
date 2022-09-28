5c703f888fdf8633f5df682a48e5c7a6c6f03e68
Prateek Gupta <prateek.gupta25apr@gmail.com>
2022-09-29 00:43:52 +0530

    Updated RecipesBinding.kt, RecipesRowBinding.kt, favorite_recipes_row_layout.xml, fragment_recipes.xml and recipes_row_layout.xml

--- Foody/app/src/main/java/prateek_gupta/foody/bindingsadapters/RecipesBinding.kt
+++ Foody/app/src/main/java/prateek_gupta/foody/bindingsadapters/RecipesBinding.kt
@@ -3,6 +3,7 @@ package prateek_gupta.foody.bindingsadapters
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.view.isVisible
 import androidx.databinding.BindingAdapter
 import prateek_gupta.foody.data.database.entities.RecipesEntity
 import prateek_gupta.foody.models.FoodRecipe
@@ -11,36 +12,21 @@ import prateek_gupta.foody.util.NetworkResult
 class RecipesBinding {
     companion object {
 
-        @BindingAdapter("readApiResponse", "readDatabase", requireAll = true)
+        @BindingAdapter("readApiResponse","readDatabase",requireAll = true)
         @JvmStatic
-        fun errorImageViewVisibility(
-            imageView: ImageView,
+        fun handleReadDataErrors(
+            view: View,
             apiResponse: NetworkResult<FoodRecipe>?,
             database: List<RecipesEntity>?
-        ) {
-            if (apiResponse is NetworkResult.Error && database.isNullOrEmpty()) {
-                imageView.visibility = View.VISIBLE
-            } else if (apiResponse is NetworkResult.Loading) {
-                imageView.visibility = View.INVISIBLE
-            } else if (apiResponse is NetworkResult.Success) {
-                imageView.visibility = View.INVISIBLE
-            }
-        }
-
-        @BindingAdapter("readApiResponse2", "readDatabase2", requireAll = true)
-        @JvmStatic
-        fun errorTextViewVisibility(
-            textView: TextView,
-            apiResponse: NetworkResult<FoodRecipe>?,
-            database: List<RecipesEntity>?
-        ) {
-            if (apiResponse is NetworkResult.Error && database.isNullOrEmpty()) {
-                textView.visibility = View.VISIBLE
-                textView.text = apiResponse.message.toString()
-            } else if (apiResponse is NetworkResult.Loading) {
-                textView.visibility = View.INVISIBLE
-            } else if (apiResponse is NetworkResult.Success) {
-                textView.visibility = View.INVISIBLE
+        ){
+            when (view){
+                is ImageView ->{
+                    view.isVisible = apiResponse is NetworkResult.Error && database.isNullOrEmpty()
+                }
+                is TextView ->{
+                    view.isVisible = apiResponse is NetworkResult.Error && database.isNullOrEmpty()
+                    view.text = apiResponse?.message.toString()
+                }
             }
         }
 
