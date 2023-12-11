717bb24d8050ac5e3cd179ad584a0001935a157b
Prateek Gupta <prateek.gupta25apr@gmail.com>
2023-12-11 21:44:52 +0530

    RecyclerView Animation

---
+++ PhysicalDeviceTester/app/src/main/java/prateek_gupta/physical_device_tester/Adapter.java
@@ -0,0 +1,56 @@
+package prateek_gupta.physical_device_tester;
+
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.view.animation.AnimationUtils;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.Arrays;
+import java.util.List;
+
+import prateek_gupta.physical_device_tester.databinding.ItemBinding;
+
+public class Adapter extends RecyclerView.Adapter<Adapter.AdapterViewHolder> {
+
+    LinearLayoutManager layoutManager;
+
+
+    public Adapter(LinearLayoutManager layoutManager) {
+        this.layoutManager = layoutManager;
+    }
+
+    List<Integer> colors = Arrays.asList(Color.BLUE, Color.RED, Color.GREEN, Color.MAGENTA, Color.CYAN,Color.BLUE, Color.RED, Color.GREEN, Color.MAGENTA, Color.CYAN);
+    @NonNull
+    @Override
+    public AdapterViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new AdapterViewHolder(ItemBinding.inflate(LayoutInflater.from(parent.getContext()),parent,false));
+    }
+
+    @Override
+    public int getItemCount() {
+        return colors.size();
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull AdapterViewHolder holder, int position) {
+        holder.itemBinding.getRoot().setBackgroundColor(colors.get(position));
+        if (position>layoutManager.findLastVisibleItemPosition())
+            holder.itemBinding.getRoot().startAnimation(AnimationUtils.loadAnimation(holder.itemBinding.getRoot().getContext(), R.anim.move_up));
+        else
+            holder.itemBinding.getRoot().startAnimation(AnimationUtils.loadAnimation(holder.itemBinding.getRoot().getContext(), R.anim.move_down));
+    }
+
+    static class AdapterViewHolder extends RecyclerView.ViewHolder{
+
+        ItemBinding itemBinding;
+
+        public AdapterViewHolder(ItemBinding itemBinding) {
+            super(itemBinding.getRoot());
+            this.itemBinding=itemBinding;
+        }
+    }
+}
--- PhysicalDeviceTester/app/src/main/java/prateek_gupta/physical_device_tester/MainActivity.java
+++ PhysicalDeviceTester/app/src/main/java/prateek_gupta/physical_device_tester/MainActivity.java
@@ -12,156 +12,36 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.splashscreen.SplashScreen;
 import androidx.core.splashscreen.SplashScreenViewProvider;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import java.util.HashMap;
 
 import coil.Coil;
 import coil.ImageLoader;
-import coil.disk.DiskCache;
 import coil.memory.MemoryCache;
 import coil.request.CachePolicy;
 import coil.request.ImageRequest;
 import coil.target.Target;
-import prateek_gupta.physical_device_tester.databinding.ActivityMain2Binding;
+import prateek_gupta.physical_device_tester.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
-    ActivityMain2Binding binding;
+    ActivityMainBinding binding;
 
     ImageLoader imageLoader;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        SplashScreen splashScreen = SplashScreen.installSplashScreen(this);
-        splashScreen.setOnExitAnimationListener(new SplashScreen.OnExitAnimationListener() {
-            @Override
-            public void onSplashScreenExit(@NonNull SplashScreenViewProvider splashScreenViewProvider) {
-                TranslateAnimation rightTranslateAnimation=new TranslateAnimation(Animation.RELATIVE_TO_PARENT,0f,Animation.RELATIVE_TO_PARENT,-1f,Animation.RELATIVE_TO_PARENT,0,Animation.RELATIVE_TO_PARENT,0f);
-                splashScreenViewProvider.getIconAnimationDurationMillis();
-                rightTranslateAnimation.setDuration(1000);
-                //rightTranslateAnimation.setFillAfter(true);
-                rightTranslateAnimation.setAnimationListener(new Animation.AnimationListener() {
-                    @Override
-                    public void onAnimationStart(Animation animation) {
-
-                    }
-
-                    @Override
-                    public void onAnimationEnd(Animation animation) {
-                        splashScreenViewProvider.remove();
-                    }
-
-                    @Override
-                    public void onAnimationRepeat(Animation animation) {
-
-                    }
-                });
-                splashScreenViewProvider.getView().startAnimation(rightTranslateAnimation);
-
-
-            }
-        });
         super.onCreate(savedInstanceState);
-        binding=ActivityMain2Binding.inflate(getLayoutInflater());
+        binding=ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        imageLoader=Coil.imageLoader(this);
-
-
-
-        imageLoader.enqueue(new ImageRequest.Builder(this)
-                        .target(new Target() {
-                            @Override
-                            public void onStart(@Nullable Drawable drawable) {
-                                binding.swipe.setRefreshing(true);
-                            }
-
-                            @Override
-                            public void onError(@Nullable Drawable drawable) {
-
-                            }
-
-                            @Override
-                            public void onSuccess(@NonNull Drawable drawable) {
-                                binding.imageView4.setImageDrawable(drawable);
-                                binding.swipe.setRefreshing(false);
-                            }
-                        })
-                        .diskCachePolicy(CachePolicy.WRITE_ONLY)
-                        .memoryCachePolicy(CachePolicy.WRITE_ONLY)
-                        .data("https://prateekgupta.pythonanywhere.com/Projects/Gupta%20Automobiles%20Website/2.png")
-                .build());
-
-
-
-                binding.button.setOnClickListener(view -> {
-                    imageLoader.enqueue(new ImageRequest.Builder(this)
-                            .target(new Target() {
-                                @Override
-                                public void onStart(@Nullable Drawable drawable) {
-                                    binding.imageView4.setImageResource(R.drawable.logo);
-                                }
-
-                                @Override
-                                public void onError(@Nullable Drawable drawable) {
-                                    MemoryCache.Value memoryCache= imageLoader.getMemoryCache().get(new MemoryCache.Key("https://prateekgupta.pythonanywhere.com/Projects/Gupta%20Automobiles%20Website/2.png",new HashMap<>()));
-                                    if (memoryCache!=null){
-                                        binding.imageView4.setImageBitmap(memoryCache.getBitmap());
-                                    }
-                                    else
-                                        Log.d("TAG", "onError: no cache");
-
-                                    Toast.makeText(MainActivity.this,"Error",Toast.LENGTH_SHORT).show();
-                                }
-
-                                @Override
-                                public void onSuccess(@NonNull Drawable drawable) {
-                                    binding.imageView4.setImageDrawable(drawable);
-                                }
-                            })
-                            .diskCachePolicy(CachePolicy.WRITE_ONLY)
-                            .memoryCachePolicy(CachePolicy.WRITE_ONLY)
-                            .data("https://prateekgupta.pythonanywhere.com/Projects/Gupta%20Automobiles%20Website/2.png")
-                            .build());
-                });
-
-                binding.swipe.setColorSchemeColors(getResources().getColor(R.color.purple_200));
-                binding.swipe.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
-                    @Override
-                    public void onRefresh() {
-                        imageLoader.enqueue(new ImageRequest.Builder(MainActivity.this)
-                                .target(new Target() {
-                                    @Override
-                                    public void onStart(@Nullable Drawable drawable) {
-                                        binding.imageView4.setImageResource(R.drawable.logo);
-                                        binding.swipe.setRefreshing(true);
-                                    }
-
-                                    @Override
-                                    public void onError(@Nullable Drawable drawable) {
-                                        MemoryCache.Value memoryCache= imageLoader.getMemoryCache().get(new MemoryCache.Key("https://prateekgupta.pythonanywhere.com/Projects/Gupta%20Automobiles%20Website/2.png",new HashMap<>()));
-                                        if (memoryCache!=null){
-                                            binding.imageView4.setImageBitmap(memoryCache.getBitmap());
-                                        }
-                                        else
-                                            Log.d("TAG", "onError: no cache");
-
-                                        Toast.makeText(MainActivity.this,"Error",Toast.LENGTH_SHORT).show();
-                                    }
-
-                                    @Override
-                                    public void onSuccess(@NonNull Drawable drawable) {
-                                        binding.imageView4.setImageDrawable(drawable);
-                                        binding.swipe.setRefreshing(false);
-                                    }
-                                })
-                                .diskCachePolicy(CachePolicy.WRITE_ONLY)
-                                .memoryCachePolicy(CachePolicy.WRITE_ONLY)
-                                .data("https://prateekgupta.pythonanywhere.com/Projects/Gupta%20Automobiles%20Website/2.png")
-                                .build());
-                    }
-                });
+        LinearLayoutManager layoutManager=new LinearLayoutManager(this);
+        binding.recyclerView.setItemViewCacheSize(-1);
+        binding.recyclerView.setLayoutManager(layoutManager);
+        binding.recyclerView.setAdapter(new Adapter(layoutManager));
     }
 }
