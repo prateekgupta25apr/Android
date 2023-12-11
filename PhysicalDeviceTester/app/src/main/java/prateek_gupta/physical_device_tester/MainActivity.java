2b09400f220eea7610db32ef36db9be49f0bcf2a
Prateek Gupta <prateek.gupta25apr@gmail.com>
2023-12-11 21:46:52 +0530

    RecyclerView Animation

--- PhysicalDeviceTester/app/src/main/java/prateek_gupta/physical_device_tester/MainActivity.java
+++ PhysicalDeviceTester/app/src/main/java/prateek_gupta/physical_device_tester/MainActivity.java
@@ -1,36 +1,15 @@
 package prateek_gupta.physical_device_tester;
 
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.animation.Animation;
-import android.view.animation.TranslateAnimation;
-import android.widget.Toast;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.splashscreen.SplashScreen;
-import androidx.core.splashscreen.SplashScreenViewProvider;
-import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
-import java.util.HashMap;
-
-import coil.Coil;
-import coil.ImageLoader;
-import coil.memory.MemoryCache;
-import coil.request.CachePolicy;
-import coil.request.ImageRequest;
-import coil.target.Target;
 import prateek_gupta.physical_device_tester.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding binding;
-
-    ImageLoader imageLoader;
 
 
     @Override
